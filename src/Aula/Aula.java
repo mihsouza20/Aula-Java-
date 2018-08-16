a042b7468b6a94b0e42f254def0c290e172e4a49
mihsouza2906 <mih_jovemaprendiz@hotmail.com>
2018-08-16 19:28:22 -0300

    Meu arquivo java

---
+++ src/Aula/Aula.java
@@ -0,0 +1,18 @@
+package Aula; 
+
+public class Aula {
+	public static void main(String args[]) {
+		
+		//variavel do tipo primitivo inteiro
+		int numero = 10, numero2 = 2;
+		
+		/*
+		 *  + adiçao
+		 *  - subtraçao
+		 *  * multiplacaçao
+		 *  / divisao
+		 */
+		
+		System.out.print(numero * numero2);
+	}
+}
